febd908265e4dbe87b219a58f6e5e9892adf7cf0
Tom <twitchyliquid64@ciphersink.net>
2017-05-12 19:36:30 +1000

    Basic user table implemented

--- src/nexus/data/user/user.go
+++ src/nexus/data/user/user.go
@@ -3,7 +3,15 @@ package user
 import (
 	"context"
 	"database/sql"
+	"errors"
+	"strconv"
+	"time"
+
+	"golang.org/x/crypto/bcrypt"
 )
+
+// ErrUserDoesntExist is returned when a user does not exist
+var ErrUserDoesntExist = errors.New("user does not exist")
 
 // Table (user) implements the databaseTable interface.
 type Table struct{}
@@ -14,11 +22,12 @@ func (t *Table) Setup(ctx context.Context, db *sql.DB) error {
 	if err != nil {
 		return err
 	}
-	_, err = tx.Exec(`CREATE TABLE IF NOT EXISTS users (
+	_, err = tx.Exec(`
+	CREATE TABLE IF NOT EXISTS users (
 	  username STRING NOT NULL,
 	  display_name STRING,
 	  created_at TIME NOT NULL,
-	  passhash_if_no_auth_methods STRING,
+	  passhash_if_no_auth_methods BLOB,
 	);
 	`)
 	if err != nil {
@@ -28,4 +37,61 @@ func (t *Table) Setup(ctx context.Context, db *sql.DB) error {
 		return err
 	}
 	return nil
+}
+
+// GetUser returns the details of a user
+func GetUser(ctx context.Context, username string, db *sql.DB) (UID int, displayName string, createdAt time.Time, err error) {
+	res, err := db.QueryContext(ctx, `
+		SELECT id(), display_name, created_at FROM users WHERE username = $1;
+	`, username)
+	if err != nil {
+		return -1, "", time.Time{}, err
+	}
+	defer res.Close()
+
+	if !res.Next() {
+		err = ErrUserDoesntExist
+		return
+	}
+	err = res.Scan(&UID, &displayName, &createdAt)
+	return
+}
+
+// SetAuth sets the default authentication hash for a uid.
+func SetAuth(ctx context.Context, uid int, passwd string, db *sql.DB) error {
+	hash, err := bcrypt.GenerateFromPassword([]byte(passwd+"yoloSalt"+strconv.Itoa(uid)), bcrypt.DefaultCost)
+	if err != nil {
+		return err
+	}
+
+	tx, err := db.Begin()
+	if err != nil {
+		return err
+	}
+	_, err = tx.ExecContext(ctx, "UPDATE users SET passhash_if_no_auth_methods=$1 WHERE id() = $2", hash, uid)
+	if err != nil {
+		return err
+	}
+	return tx.Commit()
+}
+
+// CreateUser creates a user in the datastore.
+func CreateUser(ctx context.Context, username, displayName string, db *sql.DB) error {
+	tx, err := db.Begin()
+	if err != nil {
+		return err
+	}
+	_, err = tx.Exec(`
+	INSERT INTO
+		users (username, display_name, created_at)
+		VALUES (
+			$1,
+			$2,
+			now()
+		);
+	`, username, displayName)
+	if err != nil {
+		return err
+	}
+	return tx.Commit()
 }
--- src/nexus/maintenance-tool/main.go
+++ src/nexus/maintenance-tool/main.go
@@ -2,10 +2,15 @@ package main
 
 import (
 	"context"
+	"database/sql"
+	"errors"
 	"flag"
 	"fmt"
 	"nexus/data"
+	"nexus/data/user"
 	"os"
+	"strings"
+	"text/tabwriter"
 )
 
 func die(msg string) {
@@ -13,18 +18,105 @@ func die(msg string) {
 	os.Exit(1)
 }
 
-func main() {
+var dbFlag = flag.String("db", "dev.db", "path to the database file")
+var nameFlag = flag.String("name", "", "")
+var usernameFlag = flag.String("username", "", "")
+
+var commandTable = map[string]func(context.Context, *sql.DB) error{
+	"CREATEUSER": createUserCommand,
+	"RESETAUTH":  resetAuthCommand,
+}
+
+func printCommands() {
+	fmt.Println("Commands:")
+
+	w := tabwriter.NewWriter(os.Stdout, 8, 3, 1, '\t', tabwriter.TabIndent|tabwriter.StripEscape)
+	counter := 0
+	for command := range commandTable {
+		counter++
+		fmt.Fprint(w, command+"\t")
+		if counter%4 == 0 {
+			fmt.Fprintln(w)
+		}
+	}
+
+	if counter%4 != 0 {
+		fmt.Fprintln(w)
+	}
+	w.Flush()
+	os.Exit(1)
+}
+
+func validateCommandLine() {
 	flag.Parse()
 
 	if len(flag.Args()) < 1 {
-		die("USAGE: %s <db-path> [command]")
+		die(fmt.Sprintf("USAGE: %s [--db <db-path>] command <command-specific-args>", os.Args[0]))
 	}
-	fmt.Printf("Attempting open of db: %q: ", flag.Arg(0))
-	db, err := data.Init(context.Background(), "ql", flag.Arg(0))
+
+	_, commandExists := commandTable[strings.ToUpper(flag.Arg(0))]
+	if !commandExists {
+		fmt.Printf("Error: %q is not a valid command.\n", flag.Arg(0))
+		printCommands()
+	}
+}
+
+func main() {
+	validateCommandLine()
+	ctx := context.Background()
+
+	fmt.Printf("Attempting open of db: %q: ", *dbFlag)
+	db, err := data.Init(context.Background(), "ql", *dbFlag)
 	if err != nil {
 		die(fmt.Sprintf("ERROR!\nError: %s", err.Error()))
 	}
 	fmt.Println("OK")
 	defer db.Close()
 
+	fmt.Printf("%s: ", flag.Arg(0))
+	err = commandTable[strings.ToUpper(flag.Arg(0))](ctx, db)
+	if err != nil {
+		fmt.Println("ERROR!")
+		fmt.Println(err)
+		os.Exit(1)
+	} else {
+		fmt.Println("SUCCESS.")
+	}
+}
+
+func createUserCommand(ctx context.Context, db *sql.DB) error {
+	if *nameFlag == "" {
+		die("Error: createuser needs name flag: --name <display name>")
+	}
+	if *usernameFlag == "" {
+		die("Error: createuser needs username flag: --username <username>")
+	}
+
+	uid, displayName, createdAt, err := user.GetUser(ctx, *usernameFlag, db)
+	if err == nil {
+		fmt.Println("Error: User already exists!")
+		fmt.Printf("  uid=%d, display_name=%q, created_at=%q\n", uid, displayName, createdAt)
+		die("")
+	} else if err != user.ErrUserDoesntExist {
+		return err
+	}
+
+	return user.CreateUser(ctx, *usernameFlag, *nameFlag, db)
+}
+
+func resetAuthCommand(ctx context.Context, db *sql.DB) error {
+	if *usernameFlag == "" {
+		die("Error: resetauth needs username flag: --username <username>")
+	}
+
+	if len(flag.Arg(1)) < 6 {
+		return errors.New("Password too short (6 char minimum)")
+	}
+
+	uid, _, _, err := user.GetUser(ctx, *usernameFlag, db)
+	if err != nil {
+		return err
+	}
+
+	return user.SetAuth(ctx, uid, flag.Arg(1), db)
 }
