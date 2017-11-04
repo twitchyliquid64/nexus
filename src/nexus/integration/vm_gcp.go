7d387a3c6f4737583599aec15fc165294e7c6c49
Tom <twitchyliquid64@ciphersink.net>
2017-11-05 00:50:20 +1100

    Working on supporting GCP PubSub, API currently not working nor documented.

--- src/nexus/integration/vm_gcp.go
+++ src/nexus/integration/vm_gcp.go
@@ -6,6 +6,7 @@ import (
 	"nexus/fs"
 
 	compute "google.golang.org/api/compute/v1"
+	pubsub "google.golang.org/api/pubsub/v1"
 
 	"github.com/robertkrimen/otto"
 	"golang.org/x/oauth2"
@@ -28,7 +29,7 @@ func (b *gcpInitialiser) Apply(r *Run) error {
 			return r.VM.MakeCustomError("fs-error", err.Error())
 		}
 
-		conf, err := google.JWTConfigFromJSON(b.Bytes(), compute.CloudPlatformScope)
+		conf, err := google.JWTConfigFromJSON(b.Bytes(), compute.CloudPlatformScope, pubsub.PubsubScope)
 		if err != nil {
 			return r.VM.MakeCustomError("oauth-error", err.Error())
 		}
@@ -67,7 +68,93 @@ func (b *gcpInitialiser) Apply(r *Run) error {
 		return err
 	}
 
+	if err := obj.Set("pubsub_client", func(call otto.FunctionCall) otto.Value {
+		export, err := call.Argument(0).Export()
+		if err != nil {
+			return r.VM.MakeCustomError("internal-error", err.Error())
+		}
+
+		pubsubService, err := pubsub.New(export.(*http.Client))
+		if err != nil {
+			return r.VM.MakeCustomError("internal-error", err.Error())
+		}
+
+		ret, err := makeObject(r.VM)
+		if err != nil {
+			return r.VM.MakeCustomError("internal-error", err.Error())
+		}
+		if err := b.MakePubSub(r, ret, pubsubService); err != nil {
+			return r.VM.MakeCustomError("internal-error", err.Error())
+		}
+
+		return ret.Value()
+	}); err != nil {
+		return err
+	}
+
 	return r.VM.Set("gcp", obj)
+}
+
+func (b *gcpInitialiser) MakePubSub(r *Run, obj *otto.Object, service *pubsub.Service) error {
+	if err := obj.Set("list", func(call otto.FunctionCall) otto.Value {
+		topics, err := service.Projects.Topics.List(call.Argument(0).String()).Do()
+		if err != nil {
+			return r.VM.MakeCustomError("pubsub-error", err.Error())
+		}
+		v, err := r.VM.ToValue(topics.Topics)
+		if err != nil {
+			return r.VM.MakeCustomError("internal-error", err.Error())
+		}
+		return v
+	}); err != nil {
+		return err
+	}
+
+	if err := obj.Set("create", func(call otto.FunctionCall) otto.Value {
+		topic, err := service.Projects.Topics.Create(call.Argument(0).String(), &pubsub.Topic{}).Do()
+		if err != nil {
+			return r.VM.MakeCustomError("pubsub-error", err.Error())
+		}
+		v, err := r.VM.ToValue(topic)
+		if err != nil {
+			return r.VM.MakeCustomError("internal-error", err.Error())
+		}
+		return v
+	}); err != nil {
+		return err
+	}
+
+	if err := obj.Set("publish", func(call otto.FunctionCall) otto.Value {
+		var out []*pubsub.PubsubMessage
+		jsMessages, _ := call.Argument(1).Export()
+		for _, msg := range jsMessages.([]interface{}) {
+			m := msg.(map[string]interface{})
+			output := &pubsub.PubsubMessage{
+				Data:       m["data"].(string),
+				Attributes: make(map[string]string),
+			}
+			if attrs, ok := m["attrs"].(map[string]interface{}); ok {
+				for key, val := range attrs {
+					output.Attributes[key] = val.(string)
+				}
+			}
+			out = append(out, output)
+		}
+
+		topic, err := service.Projects.Topics.Publish(call.Argument(0).String(), &pubsub.PublishRequest{Messages: out}).Do()
+		if err != nil {
+			return r.VM.MakeCustomError("pubsub-error", err.Error())
+		}
+		v, err := r.VM.ToValue(topic)
+		if err != nil {
+			return r.VM.MakeCustomError("internal-error", err.Error())
+		}
+		return v
+	}); err != nil {
+		return err
+	}
+
+	return nil
 }
 
 func (b *gcpInitialiser) MakeCompute(r *Run, obj *otto.Object, service *compute.Service) error {
